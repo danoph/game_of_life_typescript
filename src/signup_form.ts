098ed4f162f22ebc75c13d774de358f27e007b44
Jason Whelehon <wheelie33@gmail.com>
2017-10-30 11:33:03 -0500

    Refactored password helper methods into inline to simplify logic and readability

--- src/signup_form.ts
+++ src/signup_form.ts
@@ -16,83 +16,64 @@ export class SignupForm {
     MIN_PASSWORD_LENGTH: 8
   };
 
-  constructor(private params: ISignupFormParams) {}
+  constructor(private params: ISignupFormParams) { }
 
   isValid(): boolean {
 
     if (!this.params.first_name.length) {
-      this.errors.first_name = [ "can't be blank" ];
+      this.errors.first_name = ["can't be blank"];
     }
 
     if (!this.params.last_name.length) {
-      this.errors.last_name = [ "can't be blank" ];
+      this.errors.last_name = ["can't be blank"];
     }
 
     if (!this.params.email.length) {
-      this.errors.email = [ "can't be blank" ];
+      this.errors.email = ["can't be blank"];
     }
-    else{
-      if(!this.isValidEmailAddress(this.params.email)){
-        this.errors.email = [ "not a valid email address"];
-      }
+    else if (!this.isValidEmailAddress(this.params.email)) {
+      this.errors.email = ["not a valid email address"];
     }
 
 
-    if(this.params.password !== this.params.password_confirmation){
-      this.pushPasswordError("does not match")
-    }
-    else{
-      if(this.params.password.length < this.AUTH_CONFIG.MIN_PASSWORD_LENGTH){
-        this.pushPasswordError("too short" );
-      }
+    this.validatePassword();
 
-      if(!this.doesPasswordContainUppercase(this.params.password)){
-        this.pushPasswordError("doesn't contain an uppercase letter");
-      }
-      
-      if(!this.doesPasswordContainLowercase(this.params.password)){
-        this.pushPasswordError("doesn't contain a lowercase letter");
-      }
-
-      if(!this.doesPasswordContainSpecialCharacter(this.params.password)){
-        this.pushPasswordError("doesn't contain a special character");
-      }
-
-    }
-    
 
     return Object.keys(this.errors).length === 0;
   }
 
-  private pushPasswordError(errorMessage:string){
-    if(!this.errors.password) this.errors.password = [];
+  private validatePassword() {
+    if (this.params.password !== this.params.password_confirmation) {
+      this.pushPasswordError("does not match")
+    }
+    else {
+      if (this.params.password.length < this.AUTH_CONFIG.MIN_PASSWORD_LENGTH) {
+        this.pushPasswordError("too short");
+      }
+
+      if (!/[A-Z]+/.test(this.params.password)) {
+        this.pushPasswordError("doesn't contain an uppercase letter");
+      }
+
+      if (!/[a-z]+/.test(this.params.password)) {
+        this.pushPasswordError("doesn't contain a lowercase letter");
+      }
+
+      if (!/[!@#$%^&*()]+/.test(this.params.password)) {
+        this.pushPasswordError("doesn't contain a special character");
+      }
+
+    }
+  }
+
+  private pushPasswordError(errorMessage: string) {
+    if (!this.errors.password) this.errors.password = [];
 
     this.errors.password.push(errorMessage);
   }
 
-  private isValidEmailAddress(email:string):boolean{
+  private isValidEmailAddress(email: string): boolean {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(email);
   }
-
-  // password format logic
-
-  private doesPasswordContainUppercase(password:string):boolean{
-    const regex = /[A-Z]+/;
-
-    return regex.test(password);
-  }
-  private doesPasswordContainLowercase(password:string):boolean{
-    const regex = /[a-z]+/;
-
-    return regex.test(password);
-  }
-  private doesPasswordContainSpecialCharacter(password:string):boolean{
-    const regex = /[!@#$%^&*()]+/;
-
-    return regex.test(password);
-  }
-
-  // end password format logic
-
 }
