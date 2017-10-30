a48266a1b95a42cae0eb6ea20cf5e5d880d6d97f
Jason Whelehon <wheelie33@gmail.com>
2017-10-30 11:47:37 -0500

    refactored all error messages into auth_config class

--- src/signup_form.ts
+++ src/signup_form.ts
@@ -20,7 +20,17 @@ export class SignupForm {
       SPECIAL: /[!@#$%^&*()]+/,
       NUMBER: /[0-9]+/
     },
-    EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    ERROR_STATEMENTS: {
+      BLANK: "can't be blank",
+      INVALID_EMAIL: "not a valid email address",
+      PASSWORDS_DO_NOT_MATCH: "does not match",
+      PASSWORD_TOO_SHORT:  "too short",
+      PASSWORD_DOES_NOT_CONTAIN_UPPERCASE: "doesn't contain an uppercase letter",
+      PASSWORD_DOES_NOT_CONTAIN_LOWERCASE: "doesn't contain a lowercase letter",
+      PASSWORD_DOES_NOT_CONTAIN_SPECIAL_CHARACTER: "doesn't contain a special character",
+      PASSWORD_DOES_NOT_CONTAIN_NUMBER: "doesn't contain a number"
+    }
   };
 
   constructor(private params: ISignupFormParams) { }
@@ -28,18 +38,18 @@ export class SignupForm {
   isValid(): boolean {
 
     if (!this.params.first_name.length) {
-      this.errors.first_name = ["can't be blank"];
+      this.errors.first_name = [this.AUTH_CONFIG.ERROR_STATEMENTS.BLANK];
     }
 
     if (!this.params.last_name.length) {
-      this.errors.last_name = ["can't be blank"];
+      this.errors.last_name = [this.AUTH_CONFIG.ERROR_STATEMENTS.BLANK];
     }
 
     if (!this.params.email.length) {
-      this.errors.email = ["can't be blank"];
+      this.errors.email = [this.AUTH_CONFIG.ERROR_STATEMENTS.BLANK];
     }
     else if (!this.isValidEmailAddress(this.params.email)) {
-      this.errors.email = ["not a valid email address"];
+      this.errors.email = [this.AUTH_CONFIG.ERROR_STATEMENTS.INVALID_EMAIL];
     }
 
     this.validatePassword();
@@ -49,27 +59,27 @@ export class SignupForm {
 
   private validatePassword() {
     if (this.params.password !== this.params.password_confirmation) {
-      this.pushPasswordError("does not match")
+      this.pushPasswordError(this.AUTH_CONFIG.ERROR_STATEMENTS.PASSWORDS_DO_NOT_MATCH)
     }
     else {
       if (this.params.password.length < this.AUTH_CONFIG.MIN_PASSWORD_LENGTH) {
-        this.pushPasswordError("too short");
+        this.pushPasswordError(this.AUTH_CONFIG.ERROR_STATEMENTS.PASSWORD_TOO_SHORT);
       }
 
       if (!this.AUTH_CONFIG.PASSWORD_REGEX.UPPERCASE.test(this.params.password)) {
-        this.pushPasswordError("doesn't contain an uppercase letter");
+        this.pushPasswordError(this.AUTH_CONFIG.ERROR_STATEMENTS.PASSWORD_DOES_NOT_CONTAIN_UPPERCASE);
       }
 
       if (!this.AUTH_CONFIG.PASSWORD_REGEX.LOWERCASE.test(this.params.password)) {
-        this.pushPasswordError("doesn't contain a lowercase letter");
+        this.pushPasswordError(this.AUTH_CONFIG.ERROR_STATEMENTS.PASSWORD_DOES_NOT_CONTAIN_LOWERCASE);
       }
 
       if (!this.AUTH_CONFIG.PASSWORD_REGEX.SPECIAL.test(this.params.password)) {
-        this.pushPasswordError("doesn't contain a special character");
+        this.pushPasswordError(this.AUTH_CONFIG.ERROR_STATEMENTS.PASSWORD_DOES_NOT_CONTAIN_SPECIAL_CHARACTER);
       }
       
       if (!this.AUTH_CONFIG.PASSWORD_REGEX.NUMBER.test(this.params.password)) {
-        this.pushPasswordError("doesn't contain a number");
+        this.pushPasswordError(this.AUTH_CONFIG.ERROR_STATEMENTS.PASSWORD_DOES_NOT_CONTAIN_NUMBER);
       }
 
     }
